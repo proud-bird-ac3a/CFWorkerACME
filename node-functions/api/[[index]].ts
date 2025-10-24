4ebb9ab6e12837dfda728e462dae028cbe74555f
pikachuim <pikachuim@qq.com>
2025-10-24 16:36:24 +0800

    fix(package): eo node

--- node-functions/api/[[index]].ts
+++ node-functions/api/[[index]].ts
@@ -1,6 +1,6 @@
 import {app} from '../../src';
 import {serveStatic} from '@hono/node-server/serve-static';
 
-app.use("*", serveStatic({root: "./public"}));
+// app.use("*", serveStatic({root: "./public"}));
 
 export default app;
